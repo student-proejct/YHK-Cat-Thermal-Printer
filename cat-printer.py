99e30231e1338c1a33bf6a0abf5464c7b5fabcc2
Mahdi Hasanpour <mh.hasnapour@gmail.com>
2025-05-24 18:59:50 +0330

    =add pillow requiredments and make requirements

--- cat-printer.py
+++ cat-printer.py
@@ -8,33 +8,33 @@ from time import sleep
 import struct
 
 
-printerMACAddress = 'D1:DE:4C:DD:9D:56'
+printerMACAddress = 'XX:XX:XX:XX:XX:XX'
 printerWidth = 384
 port = 2
 
 
 def initilizePrinter(soc):
     soc.send(b"\x1b\x40")
-    
+
 def getPrinterStatus(soc):
     soc.send(b"\x1e\x47\x03")
-    return soc.recv(38) 
-    
+    return soc.recv(38)
+
 def getPrinterSerialNumber(soc):
     soc.send(b"\x1D\x67\x39")
     return soc.recv(21)
-    
+
 def getPrinterProductInfo(soc):
     soc.send(b"\x1d\x67\x69")
     return soc.recv(16)
-    
+
 def sendStartPrintSequence(soc):
-    soc.send(b"\x1d\x49\xf0\x19")   
-  
+    soc.send(b"\x1d\x49\xf0\x19")
+
 def sendEndPrintSequence(soc):
     soc.send(b"\x0a\x0a\x0a\x0a")
-    
-    
+
+
 def trimImage(im):
     bg = PIL.Image.new(im.mode, im.size, (255,255,255))
     diff = PIL.ImageChops.difference(im, bg)
@@ -46,7 +46,7 @@ def trimImage(im):
 def create_text(text, font_name="Lucon.ttf", font_size=12):
     img = PIL.Image.new('RGB', (printerWidth, 5000), color = (255, 255, 255))
     font = PIL.ImageFont.truetype(font_name, font_size)
-    
+
     d = PIL.ImageDraw.Draw(img)
     lines = []
     for line in text.splitlines():
@@ -72,46 +72,46 @@ def printImage(soc, im):
         # image is wider than printer resolution; scale it down proportionately
         height = int(im.height * (printerWidth / im.width))
         im = im.resize((printerWidth, height))
-        
+
     if im.width < printerWidth:
         # image is narrower than printer resolution; pad it out with white pixels
         padded_image = PIL.Image.new("1", (printerWidth, im.height), 1)
         padded_image.paste(im)
         im = padded_image
-        
-    
-    #im = im.rotate(180) #print it so it looks right when spewing out of the mouth
-    
+
+
+    im = im.rotate(180) #print it so it looks right when spewing out of the mouth
+
     # if image is not 1-bit, convert it
     if im.mode != '1':
         im = im.convert('1')
-        
-        
+
+
     # if image width is not a multiple of 8 pixels, fix that
     if im.size[0] % 8:
-        im2 = Image.new('1', (im.size[0] + 8 - im.size[0] % 8, 
+        im2 = Image.new('1', (im.size[0] + 8 - im.size[0] % 8,
                         im.size[1]), 'white')
         im2.paste(im, (0, 0))
         im = im2
-        
-        
-        
+
+
+
     # Invert image, via greyscale for compatibility
     #  (no, I don't know why I need to do this)
     im = PIL.ImageOps.invert(im.convert('L'))
     # ... and now convert back to single bit
     im = im.convert('1')
 
-    buf = b''.join((bytearray(b'\x1d\x76\x30\x00'), 
-                                          struct.pack('2B', int(im.size[0] / 8 % 256), 
-                                                      int(im.size[0] / 8 / 256)), 
-                                                      struct.pack('2B', int(im.size[1] % 256), 
-                                                                  int(im.size[1] / 256)), 
+    buf = b''.join((bytearray(b'\x1d\x76\x30\x00'),
+                                          struct.pack('2B', int(im.size[0] / 8 % 256),
+                                                      int(im.size[0] / 8 / 256)),
+                                                      struct.pack('2B', int(im.size[1] % 256),
+                                                                  int(im.size[1] / 256)),
                                                                   im.tobytes()))
-    initilizePrinter(soc)  
-    #sleep(.5)    
-    #sendStartPrintSequence(soc)
-    #sleep(.5)
+    initilizePrinter(soc)
+    sleep(.5)
+    sendStartPrintSequence(soc)
+    sleep(.5)
     soc.send(buf)
     sleep(.5)
     sendEndPrintSequence(soc)
@@ -133,7 +133,7 @@ sleep(0.5)
 img = PIL.Image.open("Turtle.jpg")
 
 #Create image from text
-text = "Line 1\nLine 2\nLine 3"
+#text = "Line 1\nLine 2\nLine 3"
 #img = create_text(text,font_size=65)
 
 
